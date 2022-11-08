5200626d61528a5f997e9d6a09d050c6154662af
Chrystian Ruan <chrystianr37@gmail.com>
2022-11-07 21:07:00 -0300

    deixando o cód mais limpo

--- script.js
+++ script.js
@@ -1,91 +1,145 @@
 let task = document.getElementById("task");
 let show = document.getElementById("show");
+let container = document.getElementById("container")
 let taskInput = document.getElementById("task-input");
 let taskBtn = document.getElementById("task-btn");
-let alert = document.getElementById("alert");
-var taskList = [];
-var arroz = document.getElementById("arroz");
-let mostra = document.getElementById("mostra");
+let divAlert = document.getElementById("alert");
+let taskList = [];
+let modal = document.getElementById("modal");
+let editInput = document.getElementById("edit-input");
 let accomplished = document.getElementById("accomplished");
 
 
-taskBtn.addEventListener("click", function() {
+function saveTask() {
     show.style.display = "block";
-    if (taskInput.value == "") {
-        alert.style.display = "block";
-        alert.textContent = "O campo está vazio";
+    if (taskInput.value.trim() == 0) {
+        divAlert.style.display = "block";
+        divAlert.textContent = "O campo está vazio";
         setTimeout(function() {
-            alert.style.display="none";
+            divAlert.style.display="none";
         }, 1500)
-    } else if(taskList.indexOf(taskInput.value) != -1) {
-        alert.style.display = "block";
-        alert.textContent = "A tarefa digitada já existe";
+    } else if(taskList.find(value => value.name == taskInput.value)) {
+        divAlert.style.display = "block";
+        divAlert.textContent = "A tarefa digitada já existe";
         taskInput.value = '';
         setTimeout(function() {
-            alert.style.display="none";
+            divAlert.style.display="none";
         }, 1500)
     } else { 
-        alert.style.display = "none";
-        taskList.push(taskInput.value);
+        divAlert.style.display = "none";
+        let value = {
+            "id": "#"+Math.floor(Date.now() * Math.random()).toString(36),
+            "name": taskInput.value,
+            "status": 1
+        };
+
+        taskList.push(value);
+      
+        taskInput.value = '';
+        console.log(taskList);
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+    }
+
+    return taskList;
+
+        
+}
+taskBtn.addEventListener("click", saveTask)
+taskBtn.addEventListener("click", showTasks)
+taskBtn.addEventListener("click", taskRealized)
+taskBtn.addEventListener("click", deleteTask)
+
+
+let divFlexs = document.getElementsByClassName("div-flex")
+
+function showTasks() {
+    for (i = 0; i < divFlexs.length; i++) {
+        divFlexs[i].style.display = "none";
+    }   
+    taskList.forEach(task => {
         let list = document.createElement("input");
         let label = document.createElement("label");
         let div = document.createElement("div");
-        let btnEdit = document.createElement("i");
         let btnDel = document.createElement("i");
         let br = document.createElement("br");
-        list.id = taskList.indexOf(taskInput.value);
+        list.id = task.id;
         list.type = "checkbox";
         list.className = "check";
         div.className = "div-flex";
-        div.id = taskInput.value;
+        show.style.display = "block";
+        div.id = task.id;
         div.style.fontSize = "22px";
-        list.name = taskInput.value;
+        list.name = task.id;
         label.className = "label";
-        label.id = taskInput.value;
-        label.textContent = taskInput.value;
-        btnEdit.id = taskInput.value;
-        btnDel.id = taskInput.value;
-        btnEdit.className = "fa fa-pencil";
+        label.id = task.id;
+        label.textContent = task.name;
+        btnDel.id = task.id;
         btnDel.className = "fa fa-trash";
+        container.appendChild(show)
         show.appendChild(div);
-        div.appendChild(btnEdit);
         div.appendChild(btnDel);
         div.appendChild(label);
         label.appendChild(list);
-    
+
         div.appendChild(br);
 
-        let checks = document.querySelectorAll(".check");
-        let labels = document.querySelectorAll(".label");
-        let deletes = document.querySelectorAll(".fa.fa-trash");
-        let edits = document.querySelectorAll(".fa.fa-pencil");
-        let divs = document.querySelectorAll(".div-flex");
+        if (task.status == 2) {
+            label.style.textDecoration = "line-through";
+            label.style.color = "#ccc";
+            list.checked = true;
+        } else { 
+            label.style.textDecoration = "none";
+            label.style.color = "black";
+        }
+    })
 
+}
+
+
+let closeModal = document.getElementById("close-modal");
+
+closeModal.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+
+
+function taskRealized() { 
+
+    let checks = document.querySelectorAll(".check");
+    let labels = document.querySelectorAll(".label");
         for (let ch of checks) {
             ch.addEventListener("click", function () { 
+                
                 if (ch.checked) {
-                    
-                        for (let lab of labels) {
-                            if (lab.id == ch.name) {
-                                lab.style.textDecoration = "line-through";
-                                lab.style.color = "#ccc";
-                                accomplished.style.display = "block";
-                                accomplished.style.backgroundColor = "chartreuse";
-                                accomplished.style.color = "black";
-                                accomplished.textContent = `A tarefa "${lab.id}" foi marcada como realizada!`;
-                                setTimeout(function() {
-                                    accomplished.style.display="none";
-                                }, 1500)
-                            } 
+                    item = taskList.find(value => value.id == ch.id);
+                    item.status = 2;
+                    for (let lab of labels) {
+                        if (lab.id == ch.name) {
+                            lab.style.textDecoration = "line-through";
+                            lab.style.color = "#ccc";
+                            accomplished.style.display = "block";
+                            accomplished.style.backgroundColor = "chartreuse";
+                            accomplished.style.color = "black";
+                            accomplished.textContent = `A tarefa "${item.name}" foi marcada como realizada!`;
+                            setTimeout(function() {
+                                accomplished.style.display="none";
+                            }, 1500)
+                            
+                           
+
+                        } 
                         
                     }
                 } else {
+                    item = taskList.find(value => value.id == ch.id);
+                    item.status = 1;
                     for (let lab of labels) {
                         if (lab.id == ch.name) {
                             lab.style.textDecoration = "none";
                              lab.style.color = "black";
                              accomplished.style.display = "block";
-                             accomplished.textContent = `A tarefa "${lab.id}" foi desmarcada como realizada.`;
+                             accomplished.textContent = `A tarefa "${item.name}" foi desmarcada como realizada.`;
                              accomplished.style.backgroundColor = "blue";
                              accomplished.style.color = "white";
                              setTimeout(function() {
@@ -95,31 +149,28 @@ taskBtn.addEventListener("click", function() {
                         }
                     }
                 }
-            console.log(ch.name)
+            console.log(item)
         
             });
-        }  
-         
-        for (let i = 0; i < deletes.length; i++) {
-            deletes[i].addEventListener("click", function() {
-                for (let j = 0; j < divs.length; j++) {
-                    if (deletes[i].id == divs[j].id) {
-                        divs[j].style.display = "none";
-                        taskList.splice(taskList.indexOf(divs[j], 1));
-                    }
-                }
+        }    
+}  
+
+
+    
+
+function deleteTask() {   
+    let deletes = document.querySelectorAll(".fa.fa-trash");
+        for (let del of deletes) {
+            del.addEventListener("click", function() {
+                item =  taskList.map(value => value.id).indexOf(del.id);
+                taskList.splice(item, 1);
+                showTasks();
+                taskBtn.click();
+                divAlert.style.display = "none";
             });
 
-        }
-
-        taskInput.value = '';
-       
-    }
-    console.log(taskList);
-
-   
-
-}); 
-
+        } 
+        
+}
 
 
